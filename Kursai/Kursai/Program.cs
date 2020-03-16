48a99ae2a7404edce6f7cca7e350611702282802
Aurimas_Puodziukas <aurimas.puodziukas@gmail.com>
2020-03-17 01:59:13 +0200

    Max function added.

--- Kursai/Kursai/Program.cs
+++ Kursai/Kursai/Program.cs
@@ -6,7 +6,30 @@ namespace Kursai
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int test = Max(3, 6, 9);
+            Console.WriteLine(test);
+
+
+            Console.ReadKey();
+
+            
+        }
+
+        static int Max(int number1, int number2, int number3)
+        {
+            if (number1 > number2 && number1 > number3)
+            {
+                return number1;
+            }
+            else if (number2 > number1 && number2 > number3)
+            {
+                return number2;
+            }
+            else
+            {
+                return number3;
+            }
         }
     }
 }
